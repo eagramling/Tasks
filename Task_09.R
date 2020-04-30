cebb89840a839a5409c1598abe7f030e423ae5a1
eagramling <59898424+eagramling@users.noreply.github.com>
2020-04-30 13:03:23 -0400

    <Elizabeth Gramling> Updated Task_09.R upload
    I wasn't able to edit my last task 09 upload so I just reuploaded the entire thing. Sorry, I was getting fed up trying to get it to work with me. Anyways, this is an updated, full-length version of Task 09. Thanks

--- Task_09.R
+++ Task_09.R
@@ -1,10 +1,18 @@
 library(phytools)
-tree <- read.tree 
+Reading in previous
+tree <- read.tree("http://www.phytools.org//Cordoba2017//data//Anolis.tre") 
 plot(tree, type = "fan")
 data <- read.csv("https://jonsmitchell.com/data/svl.csv", stringsAsFactors = F, row.names = 1)
-
+# Q2 : dimensions are 100 x 1
+dim(data)
 svl <- setNames(data$svl, rownames(data))
 Ancestors <- fastAnc(tree, svl, vars = TRUE, CI = TRUE)
+head(data)
+range(svl)
+fit <- fastAnc(tree, svl, vars = TRUE, CI = TRUE)
+print(fit, printlen = 10)
+# Q3 : est. values are "objects
+# Q4 : ancestral traits uncertainty and probability of passing the traits 
 
 par(mar = c(0.1, 0.1, 0.1, 0.1))
 plot(tree, type = "fan", lwd = 2, show.label = F)
@@ -17,8 +25,9 @@ plot(obj, type = "fan", legend =  0.7 * max(nodeHeights(tree)), sig = 2, fsize =
  
 fossilData <- data.frame(svl = log(c(25.4, 23.2, 17.7, 19.7, 24, 31)), tip1 = c("aliniger", "aliniger", "occultus", "christophei", "cristatellus", "occultus"), tip2 = c("chlorocyanus", "coelestinus", "monticola", "cybotes", "angusticeps", "angusticeps"))
 
-fossilNodes <- c()
-nodeN <- c()
+fossilNodes <- c([i, tip1], [1, tip2])
+fossilNodes <- c((ti[1 = c("aliniger", "aliniger", "occultus", "christophei", "cristatellus", "occultus"), tip2 = c("chlorocyanus", "coelestinus", "monticola", "cybotes", "angusticeps", "angusticeps"))
+nodeN <- c(fossilNodes)
 
 Node <- fastMRCA(tree, fossilData[i, "tip1"], fossilData[i, "tip2"])
 fossilNodes[i] <- fossilData[i, "svl"]
