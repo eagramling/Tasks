524a6c4768bb4d918226e1427081324ba00f2abb
eagramling <59898424+eagramling@users.noreply.github.com>
2020-04-30 12:41:43 -0400

    <Elizabeth Gramling> Updated Task_08.R Upload

--- Task_08.R
+++ Task_08.R
@@ -1,11 +1,14 @@
+library(phytools)
 text.string <- "(((((((cow, pig), whale), (bat, (lemur, human))), (robin, iguana)), coelacanth), (gold_fish, trout)), shark);"
 vert.tree <- read.tree(text = text.string)
 plot(vert.tree, edge.width = 2)
 
 nodelabels(frame = "circle", bg = 'white', cex = 1)
+#Q1 : sharks
 
 vert.tree
 str(vert.tree)
+#Q2 : nope. not lengths 
 
 tree <- read.tree(text = "(((A, B), (C, D)), E);")
 plotTree(tree, offset = 1)
@@ -29,5 +32,20 @@ plot(AnolisTree, cex = 0.25)
 Labs <- sapply(AnolisTree$edge.length, round, digits = 2)
 edgelabels(text = Labs, cex = 0.25)
 
+#Q4 :
+roundPhylogram(vert.tree)
+
+#Q5 :
+tiplabels(frame = "circle", bg = 'red', cex = 1)
+
+#Q6-Q9 :
+Lengths <- vert.tree
+names(Lengths) <- vert.tree
+names(Lengths)[which(Lengths == min(Lengths))]
+
 ltt(AnolisTree)
 abline(0, 1, lwd = 2, col = 'red', lty = 2)
+
+#Q10 :
+fit.bd(AnolisTree, rho = 0.2)
+install.packages("treebase")
