90b80d1f9972768afdc2df01a7a3e4a2d668ee67
eagramling <59898424+eagramling@users.noreply.github.com>
2020-04-30 12:14:41 -0400

    <Elizabeth Gramling> Upload Task_10.R

---
+++ Task_10.R
@@ -0,0 +1,6 @@
+library(phytools)
+desired_length <- 100
+trees <- vector(mode = "list", length = desired_length)
+births <- rep (NA, desired_length)
+fractions <- rep(NA, desired_length)
+str(trees)
